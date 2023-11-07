789152b82447ebf28b2017e9389927d0de327611
Your name <Your email>
2023-11-07 13:16:24 +0100

    route trip : ajout de conditions si trajet inexistant

--- routes/trip.js
+++ routes/trip.js
@@ -5,8 +5,17 @@ const Trip = require ('../models/trips');
 
 //GET all trips by departure and arrival
 router.get('/', function(req, res, next) {
-  Trip.find({departure: req.body.departure , arrival: req.body.arrival, date: req.body.date})
-  .then(data => { res.json({trips: data})})
+  Trip.find({departure: { $regex: new RegExp(req.body.departure) } , arrival: { $regex: new RegExp(req.body.arrival) }, date: req.body.date})
+  .then(data => { 
+    if (!checkBody(req.body, ["departure", "arrival" , "date"])){
+      res.json({result : false, error : "Missing or empty fields"});
+  } else if (data === null){
+      res.json({result : false, error : "Trip not found"});
+  } else {
+      res.json({result : true , trips : data});
+  }
+  })
 });
+
 
 module.exports = router;
