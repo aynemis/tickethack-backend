13e97c90f6b97aee4b68407433cc4a242fbb96e3
Houss82 <houssem1982mansour@gmail.com>
2023-11-08 12:53:09 +0100

    hakathon

--- routes/selectedtrips.js
+++ routes/selectedtrips.js
@@ -8,4 +8,27 @@ router.post('/', function(req, res) {
   Selectedtrip
 });
 
+
+
+
+
+// DELETE to delete trip from database
+// router.delete('/', (req, res) => {
+//   Selectedtrip.deleteOne
+// });
+
+
+router.delete('/', (req, res) => {
+  Selectedtrip.deleteOne({
+    journey: { $regex: new RegExp(req.body.journey, "i")},time: { $regex: new RegExp(req.body.time) } ,price: { $regex: new RegExp(req.body.price) },
+  }).then(deleteDoc => {
+    if(deleteDoc.deletedCount > 0){
+      Trip.find().then(data => {
+        res.json({Trip: data})
+      })
+    }
+  })
+  });
+
+
 module.exports = router;
