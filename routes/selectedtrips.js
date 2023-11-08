23e2bcf3a3c77930100d18547ca83b119c3257ef
Your name <Your email>
2023-11-08 13:42:18 +0100

    test

--- routes/selectedtrips.js
+++ routes/selectedtrips.js
@@ -2,17 +2,31 @@ var express = require('express');
 var router = express.Router();
 
 const Selectedtrip = require ('../models/selectedtrips');
+const Trip =require ('../models/trips');
 
 //POST selected trip in database
 router.post('/', function(req, res) {
-  const newSelectedtrip =  new Selectedtrip({
-    trip: req.body.trip,
-    isPaid: false,
-  })
-  newSelectedtrip.save()
-  .then(() => {
-    res.json({result: true, message: "Ajouté au panier"})
-  })
+  Trip.findById(req.body.trip)
+  .then(trip => {
+      const newSelectedtrip =  new Selectedtrip({
+        trip: trip._id,
+        isPaid: false,
+      })
+      newSelectedtrip.save()
+      .then(() => {
+        res.json({result: true, message: "Ajouté au panier"})
+      })
+
+  })  
 });
+
+//GET all selected trips
+router.get('/cart', function (req,res){
+    Selectedtrip.find()
+    .populate('trip')
+    .then(data => {
+        res.json({trips : data})
+    })
+})
 
 module.exports = router;
